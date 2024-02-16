81579c6c69696d6548b5141c2556e9ad16e1e80d
Oleksandr1233212 <pp99241979@gmail.com>
2024-02-16 20:43:51 +0200

    third commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,37 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import TaskList from './components/TaskList.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  
   </header>
 
   <main>
-    <TheWelcome />
+    <TaskList />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+
+
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; 
+  left: 10000px;
+  
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container py-5">
+    <div class="mb-3">
+      <h1 style="white-space: nowrap;">Таск Менеджер</h1>
+    </div>
+    <div class="mb-3">
+      <input v-model="newTask" type="text" class="form-control" placeholder="Введіть назву нової задачі...">
+      <button @click="addTask" class="btn btn-primary ml-2" style="white-space: nowrap;">Добавити задачу</button>
+    </div>
+    <ul class="list-group">
+      <li v-for="(task, index) in tasks" :key="task.id" class="list-group-item d-flex justify-content-between align-items-center">
+        <div>
+          <span v-if="!task.editing">
+
+            <i class="fas" :class="task.icon"></i> {{ task.name }}
+          </span>
+          <input v-else v-model="task.name" @keyup.enter="finishEdit(task)" type="text" class="form-control" placeholder="Оновіть назву задачі...">
+        </div>
+        <div>
+          <button v-if="!task.editing" @click="editTask(task)" class="btn btn-primary btn-sm"> Update </button>
+          <button v-else @click="finishEdit(task)" class="btn btn-success btn-sm"> Save </button>
+          <button @click="removeTask(index)" class="btn btn-danger btn-sm"> Delete </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskManager',
+  data() {
+    return {
+      newTask: '',
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+      
+        const defaultIcon = 'fa-check-circle'; 
+        this.tasks.push({ id: this.tasks.length + 1, name: this.newTask, icon: defaultIcon, editing: false });
+        this.newTask = '';
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    editTask(task) {
+      this.tasks.forEach(t => { if (t.id !== task.id) t.editing = false; });
+      task.editing = true;
+    },
+    finishEdit(task) {
+      task.editing = false;
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+.d-flex {
+  display: flex;
+  align-items: center;
+}
+.mb-3 {
+  display: flex;
+  align-items: center;
+}
+
+.ml-2 {
+  margin-left: 5px;
+}
+
+.container {
+  width: 90%;
+  max-width: 1200px;
+  padding-left: 15px;
+  padding-right: 100px;
+  margin: auto;
+}
+
+</style>
