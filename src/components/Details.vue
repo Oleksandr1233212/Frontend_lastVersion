74c9ecad690c961a5d8e286191e8423c6fb35208
Oleksandr1233212 <pp99241979@gmail.com>
2024-05-03 21:01:51 +0300

    details n completion

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -3,9 +3,18 @@
       <div class="row justify-content-center">
         <div class="col-md-8">
           <div v-if="task" class="task-detail">
-            <p>Hello World</p>
             <h2>Деталі завдання {{ task.name }}</h2>
             <p><strong>Автор:</strong> {{ task.author }}</p>
+            <p>Дата створення: {{ task.data }}</p>
+            <p>Категорія: {{ task.category }}</p>
+            <p v-if='task.edited=="true"'>Завдання змінювалось</p>
+            <p v-if='task.edited=="false"'>Завдання не змінювалося</p>
+            <p v-if='task.completed=="true"'>Завдання зроблене</p>
+            <p v-if='task.completed=="true"'>Час виконання: {{ task.timecomplited }}</p>
+            <p v-if='task.completed=="false"'>Завдання не зроблене</p>
+            <p>Час створення: {{ task.added }}</p>
+            <router-link :to="`/TaskList`"><button class="btn btn-info btn-sm">Вихід до задач</button></router-link>
+
          
           </div>
         </div>
